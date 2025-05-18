357f9b3cafcbcb706ecdc317a57eb9617e7bc2ec
meibolramirez <ramirezarias.mabel@gmail.com>
2025-05-18 13:14:01 -0400

    Initial commit from local

---
+++ src/main/java/com/puppawshop/ecommerce/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.puppawshop.ecommerce;
+
+/**
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to Pup Paw Shop CLI!");
+
+	}
+
+}
